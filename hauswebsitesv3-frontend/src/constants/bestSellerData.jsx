435fdefaf963b8859432367b1e4526204ddc3be2
farhanhariss <farhanharis66@gmail.com>
2024-04-27 15:19:08 +0700

    feat: responsive, animaion, navigate menu best seller

---
+++ hauswebsitesv3-frontend/src/constants/bestSellerData.jsx
@@ -0,0 +1,43 @@
+import choco_lava_float from "../assets/images/best_seller/choco_lava_float.png";
+import drink_beng_beng_caramel from "../assets/images/best_seller/drink_beng_beng_caramel.png";
+import choco_cheese_crunchy from "../assets/images/best_seller/choco_cheese_crunchy.png";
+import choco_hazlenut_with_boba_brown_sugar from "../assets/images/best_seller/choco_hazlenut_with_boba_brown_sugar.png";
+import green_thai_tea_with_egg_pudding from "../assets/images/best_seller/green_thai_tea_with_egg_pudding.png";
+import ice_mango_mix_yakult from "../assets/images/best_seller/ice_mango_mix_yakult.png";
+import mango_cheese_silky_pudding from "../assets/images/best_seller/mango_cheese_silky_pudding.png";
+import thai_tea_with_boba_brown_sugar from "../assets/images/best_seller/thai_tea_with_boba_brown_sugar.png";
+
+export const dataBestSeller = [
+  {
+    name: "Choco Cheese Crunchy",
+    image: choco_cheese_crunchy,
+  },
+  {
+    name: "Choco Lava Float",
+    image: choco_lava_float,
+  },
+  {
+    name: "Drink Beng Beng Caramel",
+    image: drink_beng_beng_caramel,
+  },
+  {
+    name: "Choco Hazlenut with Boba Brown Sugar",
+    image: choco_hazlenut_with_boba_brown_sugar,
+  },
+  {
+    name: "Green Thai Tea with Egg Pudding",
+    image: green_thai_tea_with_egg_pudding,
+  },
+  {
+    name: "Ice Mango Mix Yakult",
+    image: ice_mango_mix_yakult,
+  },
+  {
+    name: "Mango Cheese Silky Pudding",
+    image: mango_cheese_silky_pudding,
+  },
+  {
+    name: "Thai Tea with Boba Brown Sugar",
+    image: thai_tea_with_boba_brown_sugar,
+  },
+];
--- hauswebsitesv3-frontend/src/pages/HomePage.jsx
+++ hauswebsitesv3-frontend/src/pages/HomePage.jsx
@@ -1,63 +1,72 @@
 import { FooterComponent } from "../components";
-import choco_lava_float from "../assets/images/best_seller/choco_lava_float.png";
-import drink_beng_beng_caramel from "../assets/images/best_seller/drink_beng_beng_caramel.png";
-import choco_cheese_crunchy from "../assets/images/best_seller/choco_cheese_crunchy.png";
-import choco_hazlenut_with_boba_brown_sugar from "../assets/images/best_seller/choco_hazlenut_with_boba_brown_sugar.png";
-import green_thai_tea_with_egg_pudding from "../assets/images/best_seller/green_thai_tea_with_egg_pudding.png";
-import ice_mango_mix_yakult from "../assets/images/best_seller/ice_mango_mix_yakult.png";
-import mango_cheese_silky_pudding from "../assets/images/best_seller/mango_cheese_silky_pudding.png";
-import thai_tea_with_boba_brown_sugar from "../assets/images/best_seller/thai_tea_with_boba_brown_sugar.png";
-
+import { dataBestSeller } from "../constants/bestSellerData";
+import right_arrow from "../assets/images/best_seller/right.png";
+import left_arrow from "../assets/images/best_seller/left.png";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import Slider from "react-slick";
+import React, { useState } from "react";
 const HomePage = () => {
-  const data = [
-    {
-      name: "Choco Cheese Crunchy",
-      image: choco_cheese_crunchy,
-    },
-    {
-      name: "Choco Lava Float",
-      image: choco_lava_float,
-    },
-    {
-      name: "Drink Beng Beng Caramel",
-      image: drink_beng_beng_caramel,
-    },
-    {
-      name: "Choco Hazlenut with Boba Brown Sugar",
-      image: choco_hazlenut_with_boba_brown_sugar,
-    },
-    {
-      name: "Green Thai Tea with Egg Pudding",
-      image: green_thai_tea_with_egg_pudding,
-    },
-    {
-      name: "Ice Mango Mix Yakult",
-      image: ice_mango_mix_yakult,
-    },
-    {
-      name: "Mango Cheese Silky Pudding",
-      image: mango_cheese_silky_pudding,
-    },
-    {
-      name: "Thai Tea with Boba Brown Sugar",
-      image: thai_tea_with_boba_brown_sugar,
-    },
-  ];
+  const sliderRef = React.createRef();
+  const [activeIndex, setActiveIndex] = useState(0);
 
-  const settingsCarouselMenu = {
+  function SampleNextArrow() {
+    return (
+      <img
+        src={right_arrow}
+        alt="Next"
+        className="absolute right-0 top-1/2 transform -translate-y-1/2 -translate-x-1/4 w-8 h-8 lg:h-14 lg:w-14 cursor-pointer"
+        onClick={() => sliderRef.current.slickNext()}
+      />
+    );
+  }
+
+  function SamplePrevArrow() {
+    return (
+      <img
+        src={left_arrow}
+        alt="Previous"
+        className=" absolute top-1/2 transform -translate-y-1/2 translate-x-1/4 w-8 h-8 md:h-10 md:w-10 lg:h-14 lg:w-14 cursor-pointer z-10"
+        onClick={() => sliderRef.current.slickPrev()}
+      />
+    );
+  }
+
+  var settingsCarouselMenu = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 5,
+    centerMode: true,
     slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    beforeChange: (oldIndex, newIndex) => setActiveIndex(newIndex),
     responsive: [
       {
-        breakpoint: 768, // Tablet dan mobile
+        breakpoint: 1024,
         settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          centerMode: true,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 786,
+        settings: {
+          slidesToShow: 2,
+          centerMode: false,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          centerMode: false,
           slidesToShow: 2,
           slidesToScroll: 1,
         },
@@ -68,14 +77,21 @@ const HomePage = () => {
   return (
     <>
       {/* Carousel Best Seller*/}
-      <div className="bg-[#EFDFD1] h-[100svh]">
+      <div className="bg-[#EFDFD1] h-[100svh] ">
         {/* Carousel Menu */}
-        <div className="carousel-menu">
-          <Slider {...settingsCarouselMenu}>
-            {data.map((item, index) => (
-              <div key={index}>
+        <div className="carousel-menu py-10  ">
+          <Slider ref={sliderRef} {...settingsCarouselMenu}>
+            {dataBestSeller.map((item, index) => (
+              <div
+                key={index}
+                className={
+                  index === activeIndex
+                    ? "lg:scale-110 transition-transform lg:z-20"
+                    : "transition-transform"
+                }
+              >
                 <img
-                  className="w-auto h-auto px-3 lg:h-1/2 lg:w-1/2"
+                  className="w-auto h-auto md:w-[70%] lg:w-[90%] lg:h-[90%] px-2 py-10    "
                   src={item.image}
                   alt={item.name}
                 />
