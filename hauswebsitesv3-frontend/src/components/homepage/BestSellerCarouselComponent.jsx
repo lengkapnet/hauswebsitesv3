f94992e57d9975f671f924554a37ff9cf374da70
farhanhariss <farhanharis66@gmail.com>
2024-04-29 13:23:16 +0700

    feat: promo carousel big and small (already responsive)

--- hauswebsitesv3-frontend/src/components/homepage/BestSellerCarouselComponent.jsx
+++ hauswebsitesv3-frontend/src/components/homepage/BestSellerCarouselComponent.jsx
@@ -1,16 +1,24 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { dataBestSeller } from "../../constants/bestSellerData";
+import {
+  dataBestSeller,
+  dataPromoSmall,
+  dataPromoBig,
+} from "../../constants/bestSellerData";
 import Slider from "react-slick";
 import React, { useState } from "react";
 import right_arrow from "../../assets/images/best_seller/right.png";
 import left_arrow from "../../assets/images/best_seller/left.png";
+import right_promo from "../../assets/images/best_seller/right_promo.png";
+import left_promo from "../../assets/images/best_seller/left_promo.png";
 
 const MenuCarouselComponent = () => {
-  //slider for menu
-  const sliderRef = React.createRef();
+  const sliderMenuRef = React.createRef(); //slider reference for menu
+  const sliderPromoSmallRef = React.createRef(); //slider reference for promo small
+  const sliderPromoBigRef = React.createRef(); //slider reference for promo small
   //state for best menu seller
   const [activeIndex, setActiveIndex] = useState(0);
+  const [currentSlide, setCurrentSlide] = useState(0);
 
   // rigt arrow
   function SampleNextArrow() {
@@ -18,8 +26,8 @@ const MenuCarouselComponent = () => {
       <img
         src={right_arrow}
         alt="Next"
-        className="absolute right-0 top-1/2 transform -translate-y-1/2 -translate-x-1/4 w-8 h-8 lg:h-14 lg:w-14 cursor-pointer"
-        onClick={() => sliderRef.current.slickNext()}
+        className="absolute right-0 top-1/2 transform -translate-y-1/2 -translate-x-1/4 w-8 h-8 md:h-12 md:w-12 lg:h-14 lg:w-14 cursor-pointer"
+        onClick={() => sliderMenuRef.current.slickNext()}
       />
     );
   }
@@ -30,8 +38,8 @@ const MenuCarouselComponent = () => {
       <img
         src={left_arrow}
         alt="Previous"
-        className=" absolute top-1/2 transform -translate-y-1/2 translate-x-1/4 w-8 h-8 md:h-10 md:w-10 lg:h-14 lg:w-14 cursor-pointer z-10"
-        onClick={() => sliderRef.current.slickPrev()}
+        className=" absolute left-0 top-1/2 transform -translate-y-1/2 translate-x-1/4 w-8 h-8 md:h-12 md:w-12 lg:h-14 lg:w-14 cursor-pointer z-10"
+        onClick={() => sliderMenuRef.current.slickPrev()}
       />
     );
   }
@@ -63,8 +71,7 @@ const MenuCarouselComponent = () => {
         settings: {
           slidesToShow: 2,
           centerMode: false,
-          slidesToScroll: 2,
-          initialSlide: 2,
+          slidesToScroll: 1,
         },
       },
       {
@@ -78,12 +85,84 @@ const MenuCarouselComponent = () => {
     ],
   };
 
+  //carousel promo small setting
+  var settingPromoCarousel = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    centerMode: true,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 786,
+        settings: {
+          centerMode: false,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          centerMode: false,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  //carousel promo big setting
+  var settingPromoBigCarousel = {
+    infinite: true,
+    dots: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+    beforeChange: (oldIndex, newIndex) => setCurrentSlide(newIndex),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 786,
+        settings: {
+          centerMode: false,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          centerMode: false,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     // Carousel Best Seller
     <div className="bg-[#EFDFD1]">
       {/* Carousel Menu */}
-      <div className="carousel-menu py-10  ">
-        <Slider ref={sliderRef} {...settingsCarouselMenu}>
+      {/* <div className="flex  items-center w-full"> */}
+      <div className="carousel-menu">
+        <Slider ref={sliderMenuRef} {...settingsCarouselMenu}>
           {dataBestSeller.map((item, index) => (
             <div
               key={index}
@@ -94,18 +173,79 @@ const MenuCarouselComponent = () => {
               }
             >
               <img
-                className="w-auto h-auto md:w-[70%] lg:w-[90%] lg:h-[90%] px-2 py-10    "
+                className="w-auto h-auto lg:w-[90%] lg:h-[90%] px-2 py-5 lg:py-10    "
                 src={item.image}
                 alt={item.name}
               />
             </div>
           ))}
         </Slider>
-        <hr className="my-4 border-t-2 border-gray-400 sm:" />
       </div>
+
+      {/* Line separator */}
+      <div className="flex justify-center w-full">
+        <hr className=" hidden w-[90%] lg:my-4 border-t-2 border-gray-400 md:block " />
+      </div>
+
       {/* Carousel Promo 1*/}
+      <div className="carousel-promo-small lg:pt-10">
+        <Slider ref={sliderPromoSmallRef} {...settingPromoCarousel}>
+          {dataPromoSmall.map((item, index) => (
+            <div key={index}>
+              <img
+                className="w-100 h-auto px-2   "
+                src={item.image}
+                alt={item.name}
+              />
+            </div>
+          ))}
+        </Slider>
+      </div>
+
+      <div className=" flex flex-row justify-end me-10">
+        <img
+          className="w-[5%] h-[5%] hidden lg:block me-3 cursor-pointer"
+          src={left_promo}
+          alt="Left Promo"
+          onClick={() => sliderPromoSmallRef.current.slickPrev()}
+        />
+        <img
+          className="w-[5%] h-[5%] hidden lg:block cursor-pointer"
+          src={right_promo}
+          alt="Right Promo"
+          onClick={() => sliderPromoSmallRef.current.slickNext()}
+        />
+      </div>
 
       {/* Carousel Promo 2*/}
+      <div className="carousel-promo-big py-6 lg:py-10  relative">
+        <Slider
+          className=""
+          ref={sliderPromoBigRef} // Ensure sliderPromoBigRef is defined using useRef()
+          {...settingPromoBigCarousel}
+          customPaging={(i) => (
+            <div className="text-center">
+              {/* Custom paging indicator */}
+              <div
+                className={`h-2 w-2 md:w-4 rounded-full mx-auto transition-all duration-500 ease-in-out ${
+                  i === currentSlide ? "bg-gray-400 w-8" : "bg-white opacity-75"
+                }`}
+              ></div>
+            </div>
+          )}
+          dotsClass="slick-dots absolute bottom-2 w-full flex justify-center "
+        >
+          {dataPromoBig.map((item, index) => (
+            <div key={index}>
+              <img
+                className="w-[80%] h-auto mx-auto"
+                src={item.image}
+                alt={item.name}
+              />
+            </div>
+          ))}
+        </Slider>
+      </div>
     </div>
   );
 };
--- hauswebsitesv3-frontend/src/constants/bestSellerData.jsx
+++ hauswebsitesv3-frontend/src/constants/bestSellerData.jsx
@@ -6,7 +6,11 @@ import green_thai_tea_with_egg_pudding from "../assets/images/best_seller/green_
 import ice_mango_mix_yakult from "../assets/images/best_seller/ice_mango_mix_yakult.png";
 import mango_cheese_silky_pudding from "../assets/images/best_seller/mango_cheese_silky_pudding.png";
 import thai_tea_with_boba_brown_sugar from "../assets/images/best_seller/thai_tea_with_boba_brown_sugar.png";
-
+import beli_satu_gratis_satu from "../assets/images/best_seller/beli_satu_gratis_satu.png";
+import promo_bundling from "../assets/images/best_seller/promo_bundling.png";
+import promo_20_percent from "../assets/images/best_seller/promo_20_percent.png";
+import buy_one_get_one from "../assets/images/best_seller/buy_one_get_one.png";
+import spicy_double from "../assets/images/best_seller/spicy_double.png";
 export const dataBestSeller = [
   {
     name: "Choco Cheese Crunchy",
@@ -39,5 +43,31 @@ export const dataBestSeller = [
   {
     name: "Thai Tea with Boba Brown Sugar",
     image: thai_tea_with_boba_brown_sugar,
+  },
+];
+
+export const dataPromoSmall = [
+  {
+    name: "Beli Satu Gratis Satu",
+    image: beli_satu_gratis_satu,
+  },
+  {
+    name: "Promo 20 Persen",
+    image: promo_20_percent,
+  },
+  {
+    name: "Promo Bundling",
+    image: promo_bundling,
+  },
+];
+
+export const dataPromoBig = [
+  {
+    name: "Buy One Get One",
+    image: buy_one_get_one,
+  },
+  {
+    name: "Spicy Double",
+    image: spicy_double,
   },
 ];
