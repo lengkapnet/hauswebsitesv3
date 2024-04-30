441ad44ffe623f88ad93becad6c61756856fe01f
Yusuf Qisthi <yqisthi2002@gmail.com>
2024-04-30 13:35:22 +0700

    hausMobileAppComponent Reactive

--- hauswebsitesv3-frontend/src/components/homepage/HausMobileAppComponent.jsx
+++ hauswebsitesv3-frontend/src/components/homepage/HausMobileAppComponent.jsx
@@ -15,38 +15,47 @@ const donwloadAPKIos = () => {
 const HausMobileAppComponent = () => {
   return (
     <>
-      <div className="pb h-[105vh] w-full bg-[#EFDFD1] pt-10 text-[#612072]">
-        <div className="absolute -mt-20 ml-8 w-[35%]">
-          <img src={mobileMockup}></img>
-        </div>
-        <div className="flex h-[80vh] justify-end rounded-[42px] bg-[#F3B964]">
-          <div className="my-auto w-3/5">
-            <h1 className="mt-2 text-5xl font-extrabold">
-              Haus Mobile App For Order
-            </h1>
-            <p className="mt-6 text-3xl font-medium">
-              Dapatkan Voucher Menarik dan Ekstra Promo!
-            </p>
-            <div className="mt-3 gap-y-2 text-2xl font-medium">
-              <li>
-                Lebih Mudah Pesan dengan Aplikasi haus! , kamu bisa pilih
-                pesanan Delivery, Pickup, atau Dine In untuk Skip antrian
-              </li>
-              <li>Pesan Menu Favorit kamu dari toko terdekat</li>
-            </div>
-            <div className="mt-12 flex gap-x-10">
-              <img
-                src={playstore}
-                alt="Google Play Store"
-                onClick={donwloadAPK}
-                className="cursor-pointer"
-              />
-              <img
-                src={appstore}
-                alt="Apple Store"
-                onClick={donwloadAPKIos}
-                className=" cursor-pointer"
-              />
+      <div className="relative w-full justify-center bg-[#EFDFD1] py-10  text-[#612072] lg:h-[100vhvh] xl:h-[105vh]">
+        <div className="my-auto ">
+          <div className="ml-8 hidden w-[32.5%] md:absolute md:my-14 md:block  xl:my-0 xl:-mt-12">
+            <img src={mobileMockup}></img>
+          </div>
+          <div className="my-auto flex  h-screen w-full justify-center rounded-[42px] bg-[#F3B964] px-4 py-6 md:justify-end md:px-0 md:py-6 lg:h-[40vh] lg:py-2 xl:h-[80vh]">
+            <div className="ml-4 md:my-auto md:w-3/5">
+              <h1 className="mt-2 text-center text-3xl font-extrabold md:text-start md:text-5xl">
+                Haus Mobile App For Order
+              </h1>
+              <p className="mt-6 text-2xl font-medium md:text-3xl">
+                Dapatkan Voucher Menarik dan Ekstra Promo!
+              </p>
+              <div className="mt-3 gap-y-2 text-xl font-medium md:text-2xl">
+                <li>
+                  Lebih Mudah Pesan dengan Aplikasi haus! , kamu bisa pilih
+                  pesanan Delivery, Pickup, atau Dine In untuk Skip antrian
+                </li>
+                <li>Pesan Menu Favorit kamu dari toko terdekat</li>
+              </div>
+
+              <p className="my-2 text-center text-[#404040] md:hidden">
+                Download aplikasi di
+              </p>
+              <div className="flex justify-center gap-x-4 md:mt-8 md:flex md:justify-start md:gap-x-6 lg:mt-12 lg:gap-x-10">
+                <img
+                  src={playstore}
+                  alt="Google Play Store"
+                  onClick={donwloadAPK}
+                  className="h-[40px] cursor-pointer md:h-full"
+                />
+                <img
+                  src={appstore}
+                  alt="Apple Store"
+                  onClick={donwloadAPKIos}
+                  className="h-[40px] cursor-pointer md:h-full"
+                />
+              </div>
+              <div className="absolute mt-5 w-[90.5%]  md:hidden">
+                <img src={mobileMockup}></img>
+              </div>
             </div>
           </div>
         </div>
